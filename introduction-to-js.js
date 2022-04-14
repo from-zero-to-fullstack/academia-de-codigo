06de578635d6b4fcbe2ddc9d78c68f99557650a4
ma-oliveiramarques <ma_oliveiramarques@hotmail.com>
2022-04-14 19:35:30 +0100

    Solutions exercises 1 to 10
    Introduction to JS

---
+++ introduction-to-js.js
@@ -0,0 +1,63 @@
+// Exercise 1:
+var yourName = '<Beta Code Cadet>';
+
+if (!yourName) {
+    console.log('Please give me a name!');
+} else {
+    console.log('Welcome, ' + yourName + '! Let the games begin!');
+}
+
+// Exercise 2:
+console.log('Hello, friend!');
+
+// Exercise 3:
+console.log('But only what I tell it.');
+console.log('It never does quite what I want');
+console.log('I wish that they would sell it.');
+console.log('I really hate this damned machine');
+
+// Exercise 4: - How to add comment lines
+//Thoughts on Getting Better
+/* 
+console.log('The first step to getting better is believing you can get better.');
+console.log('In her book, Mindset, Dweck explains how to start talking back to your fixed mindset.');
+console.log('The fixed mindset says, “What if you fail? You’ll be a failure.” ');
+console.log('The growth mindset replies, “Most successful people had failures along the way.”');
+console.log('Now when I first heard about this work, I just thought: that’s nice, but I already do all this.');
+console.log('I believe fervently that intelligence can change and that talents can be learned.');
+console.log('Indeed, I’d say I’m almost pathologically growth mindset.');
+*/
+console.log('But even I began to notice there are some things I have a fixed mindset about.');
+
+// Exercise 5: - Negative and fractional numbers
+console.log(-15.2);
+
+// Exercise 6: - Arithmetic operators
+console.log(30 + 10 + 2);
+console.log(44 - 2);
+console.log(7 * 7 - 7);
+console.log(84 / 2);
+
+// Exercise 7: - How to calculate my age
+console.log((2022 - 1990) * 52);
+
+// Exercise 8: - Power numbers
+console.log(2 ** 8); // 256
+console.log(2 ** 10); // 1024
+console.log(2 ** 6); // 64
+console.log(2 ** 7); // 128
+
+// Exercise 9: - Use of the % operator
+console.log(20 / 7);
+
+let num1 = 20;
+let num2 = 7;
+
+let remainder = num1 % num2;
+
+console.log(remainder);
+
+// Exercise 10: - Declare a variable
+var myAge = 32;
+
+console.log(myAge);
